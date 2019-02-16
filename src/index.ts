5b29c80ec69607f185b1e80bd3be61c7c029a2df
Jude Daryl Clarino <jclarino@savvysherpa.com>
2019-02-16 20:01:58 +0800

    Update tslint

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,26 @@
-/// <reference path="./numberextensions.ts"/>
+import './numberextensions';
 export module Geographic {
   export function DistanceBetween(
-    geocoordinate_1: GeographicCoordinate,
-    geocoordinate_2: GeographicCoordinate,
+    geoCoordinate_1: IGeographicCoordinate,
+    geoCoordinate_2: IGeographicCoordinate,
   ): number {
     const earthRadiusKm = 6371;
-    let lat1 = geocoordinate_1.lat;
-    let lat2 = geocoordinate_2.lat;
-    let lon1 = geocoordinate_1.lng;
-    let lon2 = geocoordinate_2.lng;
-    var dLat = (lat2 - lat1) * (Math.PI / 180);
-    var dLon = (lon2 - lon1) * (Math.PI / 180);
+    let lat1 = geoCoordinate_1.lat;
+    let lat2 = geoCoordinate_2.lat;
+    const lon1 = geoCoordinate_1.lng;
+    const lon2 = geoCoordinate_2.lng;
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
     lat1 = lat1 * (Math.PI / 180);
     lat2 = lat2 * (Math.PI / 180);
-    var a =
+    const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return earthRadiusKm * c;
   }
-
-  export interface GeographicCoordinate {
-    lat: number;
-    lng: number;
-  }
+}
+export interface IGeographicCoordinate {
+  lat: number;
+  lng: number;
 }
